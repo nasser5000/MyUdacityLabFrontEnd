99fe0ea055632176bf8240331d7a4f9c5b13950e
nassersalemfarrag <nassersalemfarrag@gmail.com>
2020-02-14 19:20:12 +0300

    create makeGrid Function

--- designs.js
+++ designs.js
@@ -2,3 +2,15 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
+
+function makeGrid(height, width) {
+    for (let i = 0; i < height; i++) {
+        let tableRow = document.createElement("tr");
+
+        for (let j = 0; j < width; j++) {
+            let tableCell = document.createElement("td");
+            tableRow.appendChild(tableCell);
+        }
+        gameTable.appendChild(tableRow);
+    }
+}
