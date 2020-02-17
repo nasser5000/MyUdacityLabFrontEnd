a2cf4125b8f6d267c505c89d5889bbe41992af58
nassersalemfarrag <nassersalemfarrag@gmail.com>
2020-02-17 18:41:18 +0300

    create the function colorPixel

--- designs.js
+++ designs.js
@@ -3,6 +3,14 @@
 
 // When size is submitted by the user, call makeGrid()
 
+/**
+* @description creates a grid dynamically based on user input for height and width
+* @param {number} height - height of the grid in rows
+* @param {number} width - width of the grid in columns
+*
+* @references: "Traversing an HTML table with JavaScript and DOM Interfaces",
+* by Nasser Salem, Mozilla Web Docs. MDN JavaScript and DOM references. ".
+*/
 function makeGrid(height, width) {
     for (let i = 0; i < height; i++) {
         let tableRow = document.createElement("tr");
@@ -14,3 +22,27 @@ function makeGrid(height, width) {
         gameTable.appendChild(tableRow);
     }
 }
+
+function colorPixel(e){
+    colorPicker = document.querySelector("#colorPicker").value;
+    e.target.style.backgroundColor = colorPicker;
+}
+
+var start = document.getElementById('sizePicker');
+start.addEventListener('submit', function(e){
+    e.preventDefault();
+    document.getElementById("pixelCanvas").innerHTML = "";
+    // Select size input provided by the user
+    // When size is submitted by the user, call makeGrid()
+    let gridHeight = document.getElementById("inputHeight").value;
+    let gridWidth = document.getElementById("inputWidth").value;
+    makeGrid(gridHeight, gridWidth);
+
+    var pixels = document.querySelectorAll('td');
+
+    // calls the ColorPixel function when any pixel area is clicked
+    for (var i = 0; i < pixels.length; i++) {
+        pixels[i].addEventListener('click', colorPixel, false);
+    }
+
+});
